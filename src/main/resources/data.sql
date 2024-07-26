64fe9dfc1aa048ed4d1318271b5909a95b3c8b90
Oscar <oscar15-91@hotmail.com>
2024-07-26 13:04:25 +0200

    añadido crud

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,4 @@
-INSERT INTO requests (idConsult,name, dateConsult, typeConsult, description) VALUES (5, 'Oscar', '2021-04-12', 'Radiologia', 'Descripcion 1');
+INSERT INTO consults (id_consult, date_consult, description, name, type_consult) 
+VALUES 
+    (1, '2023-04-01', 'Descripcion', 'Oscar', 'Tipo consulta'),
+    (2, '2023-04-02', 'Descripcion.', 'Juan', 'Tipo consulta');
