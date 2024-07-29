eed5eb2c1bacb7f7c949a35e9219afa29068a9ae
Oscar <oscar15-91@hotmail.com>
2024-07-29 15:24:30 +0200

    cambios en sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO consults (id_consult, date_consult, description, name, type_consult) 
 VALUES 
-    (1, '2023-04-01', 'Descripcion', 'Oscar', 'Tipo consulta'),
-    (2, '2023-04-02', 'Descripcion.', 'Juan', 'Tipo consulta');
+    (default, '2023-04-01', 'Descripcion', 'Oscar', 'Tipo consulta'),
+    (default, '2023-04-02', 'Descripcion.', 'Juan', 'Tipo consulta');
